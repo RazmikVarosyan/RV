d8867412ee41b966b33ac82c64964a73fdc8fecf
Your Name <you@example.com>
2020-02-08 17:04:04 +0400

    popox

---
+++ client.js
@@ -0,0 +1,50 @@
+
+var socket = io();
+var side = 10;
+function setup() {
+    createCanvas(800, 800);
+    background('#acacac');
+}
+function nkarel(matrix) {
+    for (var y = 0; y < matrix.length; y++) {
+        for (var x = 0; x < matrix[y].length; x++) {
+            var object = matrix[y][x];
+            if (object == 1) {
+                fill("green");
+            }
+            else if (object == 0) {
+                fill('#acacac');
+            }
+            else if (object == 2) {
+                fill('yellow');
+            }
+            else if (object == 3) {
+                fill('red'); 
+            }
+            else if (object == 4) {
+                fill('blue');         
+            }
+            else if (object == 5) {
+                fill('orange');       
+            }
+            else if (object == 6) {
+                fill('lightblue');       
+            }
+            rect(x * side, y * side, side, side);
+        }
+    }
+}
+
+socket.on("Send Matrix", nkarel);
+
+
+
+function Jrhexex() {
+    socket.emit("Jrhexex")
+}
+/* function addGrass() {
+    socket.emit("")
+} */
+// function addGrassEater() {
+//     socket.emit("add grassEater")
+// } 
